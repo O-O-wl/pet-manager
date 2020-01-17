c31ad08b5ea3d4cae24dbf2925a043627d723be8
부엉이 <o.o.wl.soft@gmail.com>
2020-01-18 03:50:03 +0900

    FEAT: PetStoageService 구현체 정의

--- PetManager/PetManager/Persistance/PetStoageService.swift
+++ PetManager/PetManager/Persistance/PetStoageService.swift
@@ -12,3 +12,35 @@ protocol PetStoageService {
     func load(completion: @escaping (Result<[Pet], Error>) -> Void)
     func save(_ pets: [Pet], completion: @escaping (Result<Void, Error>) -> Void)
 }
+
+class PetStoagServiceImplementation: PetStoageService {
+    
+    // MARK: - Properties
+    
+    private let fileURL: URL = {
+        let documentURL = FileManager.default.urls(for: .documentationDirectory, in: .userDomainMask).first!
+        let fileName = "pets"
+        return documentURL.appendingPathComponent(fileName)
+    }()
+    
+    // MARK: - Methods
+    
+    func load(completion: @escaping (Result<[Pet], Error>) -> Void) {
+        do {
+            let data = try Data(contentsOf: fileURL)
+            let pets = try JSONDecoder().decode([Pet].self, from: data)
+            completion(.success(pets))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    func save(_ pets: [Pet], completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            let data = try JSONEncoder().encode(pets)
+            try data.write(to: fileURL)
+        } catch {
+            completion(.failure(error))
+        }
+    }
+}
