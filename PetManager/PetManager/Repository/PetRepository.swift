305ad9b19b7106f96460352fd8a4313636dcf394
부엉이 <o.o.wl.soft@gmail.com>
2020-01-18 08:00:30 +0900

    FEAT: Repository fetch 메소드 구현

--- PetManager/PetManager/Repository/PetRepository.swift
+++ PetManager/PetManager/Repository/PetRepository.swift
@@ -17,7 +17,11 @@ class PetRepositoryImplementation: PetRepository {
     // MARK: - Dependencies
     
     private let storageService: PetStorageService
-
+    
+    // MARK: - Properties
+    
+    private var pets: [Pet] = []
+    
     // MARK: - Initialization
     
     init(storageService: PetStorageService) {
@@ -27,7 +31,41 @@ class PetRepositoryImplementation: PetRepository {
     // MARK: - Methods
     
     func fetch(at indexPath: IndexPath, completion: @escaping (Result<Pet, Error>) -> Void) {
-        ()
+        let index = indexPath.row
+        if pets.count <= index {
+            reload(completion: { result in
+                switch result {
+                case .success(_):
+                    if self.pets.count > index {
+                        completion(.success(self.pets[index]))
+                    } else {
+                        completion(.failure(RepositoryError.outOfRange))
+                    }
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            })
+        } else {
+            completion(.success(self.pets[index]))
+        }
     }
     
+    private func reload(completion: @escaping (Result<Void, Error>) -> Void) {
+        storageService.load { [weak self] result in
+            switch result {
+            case .success(let pets):
+                self?.pets = pets
+                completion(.success(()))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+}
+
+// MARK: - RepositoryError
+
+enum RepositoryError: Error {
+    case outOfRange
 }
