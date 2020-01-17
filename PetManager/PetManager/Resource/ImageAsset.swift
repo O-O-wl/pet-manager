606f1dd8591d8ca9ccea81444e5120a24957b12a
부엉이 <o.o.wl.soft@gmail.com>
2020-01-18 04:46:05 +0900

    FEAT: ImageAsset.Name 네임스페이스에 상수 선언

---
+++ PetManager/PetManager/Resource/ImageAsset.swift
@@ -0,0 +1,18 @@
+//
+//  UIImage+.swift
+//  PetManager
+//
+//  Created by 이동영 on 2020/01/17.
+//  Copyright © 2020 이동영. All rights reserved.
+//
+
+import UIKit
+
+enum ImageAsset {
+    enum Name {
+        static let dog = "dog"
+        static let cat = "cat"
+        static let parrot = "parrot"
+        static let lizard = "lizard"
+    }
+}
