b9374b7034d593816070bd91c94024cfa3b48e40
부엉이 <o.o.wl.soft@gmail.com>
2020-01-17 18:54:22 +0900

    TEST: Pet 초기화 테스트 추가

---
+++ PetManager/PetManagerTests/Cases/Entity/PetTests.swift
@@ -0,0 +1,34 @@
+//
+//  PetTests.swift
+//  PetManagerTests
+//
+//  Created by 이동영 on 2020/01/17.
+//  Copyright © 2020 이동영. All rights reserved.
+//
+@testable import PetManager
+import XCTest
+
+class PetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_pet_initialzation() {
+        // given
+        let name = "나르"
+        let type = Animal.dog
+        
+        // when
+        let pet = Pet(name: name, type: type)
+        
+        // then
+        XCTAssertEqual(pet.name, name)
+        XCTAssertEqual(pet.type, type)
+    }
+
+}
