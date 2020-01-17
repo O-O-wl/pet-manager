7bdeb2835be10978e5aab387f5832a2c55012842
부엉이 <o.o.wl.soft@gmail.com>
2020-01-18 05:20:13 +0900

    REACTOR: Animal 모델에 대한 테스트 코드 추가

--- PetManager/PetManagerTests/Cases/Entity/AnimalTests.swift
+++ PetManager/PetManagerTests/Cases/Entity/AnimalTests.swift
@@ -26,31 +26,31 @@ class AnimalTests: XCTestCase {
         // given
         let name = "강아지"
         let cryingSound = "멍 - 멍 -"
-        let imageData = Data()
+        let profileImageAssetName = "dog"
         
         // when
         let animal = Animal(name: name,
                             cryingSound: cryingSound,
-                            profileImageData: imageData)
+                            profileImageAssetName: profileImageAssetName)
         // then
         XCTAssertEqual(animal.name, name)
         XCTAssertEqual(animal.cryingSound, cryingSound)
-        XCTAssertEqual(animal.profileImageData, imageData)
+        XCTAssertEqual(animal.profileImageAssetName, profileImageAssetName)
     }
 
     func test_animal_initalization_when_cryingSound_is_nil() {
         // given
         let name = "도마뱀"
         let cryingSound: String? = nil
-        let imageData = Data()
+        let profileImageAssetName = "lizard"
         
         // when
         let animal = Animal(name: name,
                             cryingSound: cryingSound,
-                            profileImageData: imageData)
+                            profileImageAssetName: profileImageAssetName)
         // then
         XCTAssertEqual(animal.name, name)
         XCTAssertNil(animal.cryingSound)
-        XCTAssertEqual(animal.profileImageData, imageData)
+        XCTAssertEqual(animal.profileImageAssetName, profileImageAssetName)
     }
 }
