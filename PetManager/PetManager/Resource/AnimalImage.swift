4e2abd21cbe6687d6564db90d60e4c6f5d3bd384
부엉이 <o.o.wl.soft@gmail.com>
2020-01-17 18:16:31 +0900

    FEAT: Asset 초기화한 static image instance 초기화

---
+++ PetManager/PetManager/Resource/AnimalImage.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+.swift
+//  PetManager
+//
+//  Created by 이동영 on 2020/01/17.
+//  Copyright © 2020 이동영. All rights reserved.
+//
+
+import UIKit
+
+enum AnimalImage {
+    static let dog = #imageLiteral(resourceName: "dog")
+    static let cat = #imageLiteral(resourceName: "cat")
+    static let parrot = #imageLiteral(resourceName: "parrot")
+    static let lizard = #imageLiteral(resourceName: "lizard")
+}
+
