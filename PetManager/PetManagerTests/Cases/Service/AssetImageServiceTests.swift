52c68c7dcb9ae8256666a20364863337448e70cb
부엉이 <o.o.wl.soft@gmail.com>
2020-01-18 12:06:52 +0900

    FEAT: AssetImageService 구현

--- PetManager/PetManagerTests/Cases/Service/AssetImageServiceTests.swift
+++ PetManager/PetManagerTests/Cases/Service/AssetImageServiceTests.swift
@@ -5,7 +5,7 @@
 //  Created by 이동영 on 2020/01/18.
 //  Copyright © 2020 이동영. All rights reserved.
 //
-
+@testable import PetManager
 import XCTest
 
 class AssetImageServiceTests: XCTestCase {
@@ -22,5 +22,27 @@ class AssetImageServiceTests: XCTestCase {
         sut = nil
         
         super.tearDown()
+    }
+    
+    func test_fetch_image_success() {
+        // given
+        let assetName = "dog"
+        
+        // when
+        let image = sut.fetchImage(assetName: assetName)
+        
+        // then
+        XCTAssertEqual(image, UIImage(contentsOfFile: assetName))
+    }
+    
+    func test_fetch_image_failure_when_assetName_is_wrong() {
+        // given
+        let assetName = "d"
+        
+        // when
+        let image = sut.fetchImage(assetName: assetName)
+        
+        // then
+        XCTAssertNil(image)
     }
 }
