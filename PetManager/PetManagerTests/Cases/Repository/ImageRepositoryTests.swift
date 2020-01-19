eec10fb3b6e8115a9daa65b312ce769f9a3ea07c
부엉이 <o.o.wl.soft@gmail.com>
2020-01-19 09:31:29 +0900

    TEST: ImageRepository 테스트 케이스 셋업

--- PetManager/PetManagerTests/Cases/Repository/ImageRepositoryTests.swift
+++ PetManager/PetManagerTests/Cases/Repository/ImageRepositoryTests.swift
@@ -9,14 +9,61 @@
 import XCTest
 
 class ImageRepositoryTests: XCTestCase {
-
+    
+    var key1ForCache: String!
+    var key2ForCache: String!
+    var key1ForAssetService: String!
+    var key2ForAssetService: String!
+    
+    var fakeImageForCache: UIImage!
+    var fakeImageForAssetService: UIImage!
+    
+    var mockAssetImageService: MockAssetImageService!
+    var mockImageCacheService: MockImageCacheService!
+    
+    var sut: ImageRepositoryImplementation!
     
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        key1ForCache = "캐시키1"
+        key2ForCache = "캐시키2"
+        key1ForAssetService = "에셋키1"
+        key2ForAssetService = "에셋키2"
+        
+        fakeImageForCache = UIImage()
+        fakeImageForAssetService = UIImage()
+        
+        mockImageCacheService = MockImageCacheService(
+            registerdAssetName: key1ForCache, key1ForCache,
+            fakeImage: fakeImageForCache
+        )
+        mockAssetImageService = MockAssetImageService(
+            registerdAssetName: key1ForAssetService, key2ForAssetService,
+            fakeImage: fakeImageForAssetService
+        )
+        
+        sut = ImageRepositoryImplementation(
+            cacheService: mockImageCacheService,
+            assetImageService: mockAssetImageService
+        )
     }
-
+    
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        key1ForCache = nil
+        key2ForCache = nil
+        key1ForAssetService = nil
+        key2ForAssetService = nil
+        
+        fakeImageForCache = nil
+        fakeImageForAssetService = nil
+        
+        mockImageCacheService = nil
+        mockAssetImageService = nil
+        
+        sut = nil
+        
+        super.tearDown()
     }
-
+    
 }
