e721a2df21fe956299b5e57628c26ce77aa3a327
부엉이 <o.o.wl.soft@gmail.com>
2020-01-19 17:56:43 +0900

    FEAT: AddPetPresenter 기본 구현

--- PetManager/PetManager/Scene/AddPet/AddPetPresenter.swift
+++ PetManager/PetManager/Scene/AddPet/AddPetPresenter.swift
@@ -6,32 +6,55 @@
 //  Copyright © 2020 이동영. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct AddRequest {
     let name: String
-    let typeDescription: String
+    let typeName: String
 }
 
 protocol AddPetView: AnyObject {
-    func display(at type: String)
+}
+
+protocol AnimalTypeView {
+    func display(typeName: String)
+    func display(profileImage: UIImage)
 }
 
 protocol AddPetPresenter {
-    func requestAddition(with parameter: AddRequest)
+    var numberOfAnimalTypes: Int { get }
+    func configure(view: AddPetView)
+    func didSelectType(at index: Int)
+    func addButtonDidTap(with parameter: AddRequest)
 }
 
 class AddPetPresenterImplementation: AddPetPresenter {
+    var numberOfAnimalTypes: Int { return 0 }
+    
+    func configure(view: AddPetView) {
+        
+    }
+    
+    func didSelectType(at index: Int) {
+        
+    }
     
     private unowned let view: AddPetView
+    private let animalTypeProvider: AnimalTypeProvider
+    private let imageRepository: ImageRepository
     private let petRepository: PetRepository
     
-    init(view: AddPetView, petRepository: PetRepository) {
+    init(view: AddPetView,
+         animalTypeProvider: AnimalTypeProvider,
+         imageRepository: ImageRepository,
+         petRepository: PetRepository) {
         self.view = view
+        self.animalTypeProvider = animalTypeProvider
+        self.imageRepository = imageRepository
         self.petRepository = petRepository
     }
     
-    func requestAddition(with parameter: AddRequest) {
+    func addButtonDidTap(with parameter: AddRequest) {
         
     }
     
--- PetManager/PetManagerTests/Helper/View/MockAddPetView.swift
+++ PetManager/PetManagerTests/Helper/View/MockAddPetView.swift
@@ -5,5 +5,14 @@
 //  Created by 이동영 on 2020/01/19.
 //  Copyright © 2020 이동영. All rights reserved.
 //
-
+@testable import PetManager
 import Foundation
+
+class MockAddPetView: AddPetView {
+    
+    var displayedTypeName = ""
+    
+    func display(typeName type: String) {
+        
+    }
+}
