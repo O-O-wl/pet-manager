e1972a445abbd83ca787a21d9a142e38fcfc8287
부엉이 <o.o.wl.soft@gmail.com>
2020-01-19 19:18:37 +0900

    FEAT: AnimalTypeProvider 구현

--- PetManager/PetManager/Service/Provider/AnimalProvider.swift
+++ PetManager/PetManager/Service/Provider/AnimalProvider.swift
@@ -12,3 +12,18 @@ protocol AnimalTypeProvider {
     func provideAllTypes() -> [Animal]
     func provide(for typeName: String) -> Animal?
 }
+
+
+class AnimalTypeProviderImplementation: AnimalTypeProvider {
+    
+    private let defaultTypes: [Animal] = [.dog, .cat, .parrot, .lizard]
+    
+    func provideAllTypes() -> [Animal] {
+        return defaultTypes
+    }
+    
+    func provide(for typeName: String) -> Animal? {
+        return defaultTypes.first { $0.name == typeName }
+    }
+}
+
