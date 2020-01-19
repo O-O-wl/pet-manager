fd0a03f01b2beb74cf5c8d5e09ce3d4d7b516405
부엉이 <o.o.wl.soft@gmail.com>
2020-01-19 20:27:16 +0900

    FEAT: AnimalCell 구현

--- PetManager/PetManager/Scene/AddPet/Cell/AnimalCell.swift
+++ PetManager/PetManager/Scene/AddPet/Cell/AnimalCell.swift
@@ -10,4 +10,75 @@ import UIKit
 
 class AnimalCell: UICollectionViewCell {
     
+    // MARK: - UI
+    
+    private let animalNameLabel = UILabel()
+    private let animalImageView = UIImageView()
+    
+    // MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpLayout()
+        setUpAttribute()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setUpLayout()
+        setUpAttribute()
+    }
+    
+    // MARK: - Layouts
+    
+    func setUpLayout() {
+        self.do {
+            $0.addSubview(animalNameLabel)
+            $0.addSubview(animalImageView)
+        }
+        
+        animalNameLabel.snp.makeConstraints {
+            $0.top.equalTo(animalImageView.snp.bottom).offset(5)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        animalImageView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.width.equalTo(animalImageView.snp.height)
+        }
+    }
+    
+    // MARK: - Attributes
+    
+    func setUpAttribute() {
+        self.layer.cornerRadius = 5
+        self.clipsToBounds = true
+        
+        animalImageView.do {
+            $0.contentMode = .scaleAspectFit
+            $0.clipsToBounds = true
+        }
+        
+        animalNameLabel.do {
+            $0.textAlignment = .center
+        }
+    }
 }
+
+// MARK: - AnimalView
+
+extension AnimalCell: AnimalView {
+    func display(typeName: String) {
+        animalNameLabel.text = typeName
+    }
+    
+    func display(profileImage: UIImage) {
+        animalImageView.image = profileImage
+    }
+}
+
+// MARK: - Reusable
+
+extension AnimalCell: Reusable {}
