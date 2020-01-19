3fe4700c5b1740221b17e7b8539e5646ba95293b
부엉이 <o.o.wl.soft@gmail.com>
2020-01-19 15:41:51 +0900

    TEST: AddPetPresenter Test 파일 추가

--- PetManager/PetManagerTests/Cases/Presenter/AddPetPresenterTests.swift
+++ PetManager/PetManagerTests/Cases/Presenter/AddPetPresenterTests.swift
@@ -10,24 +10,21 @@ import XCTest
 
 class AddPetPresenterTests: XCTestCase {
 
+    var mockPetRepository: MockPetRepository!
+    var sut: AddPetPresenter!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        mockPetRepository = MockPetRepository(pets: [])
+        sut = AddPetPresenterImplementation(prtRepository: mockPetRepository)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+        mockPetRepository = nil
+        sut = nil
+        
+        super.tearDown()
+    }ㅎ
 
 }
