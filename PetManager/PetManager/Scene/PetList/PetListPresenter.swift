a7a35d5a9ccf228844517635f701d3fefa7c6cfc
부엉이 <o.o.wl.soft@gmail.com>
2020-01-19 12:42:52 +0900

    FEAT: numberOfPets 정상 동작하게 구현

--- PetManager/PetManager/Scene/PetList/PetListPresenter.swift
+++ PetManager/PetManager/Scene/PetList/PetListPresenter.swift
@@ -14,19 +14,45 @@ protocol PetListPresenter {
 }
 
 
-class PetListPresenterImplementation: PetListPresenter {
+class PetListPresenterImplementation{
     
     // MARK: - Dependencies
+    
     private unowned let view: PetListView
     private let petRepository: PetRepository
     private let imageRepository: ImageRepository
     
-    var numberOfPets: Int { return 0 }
+    // MARK: - Properties
+    
+    private var pets: [Pet] = []
+    
+    // MARK: - Initialization
     
     init(view: PetListView, petRepository: PetRepository, imageRepository: ImageRepository) {
         self.view = view
         self.petRepository = petRepository
         self.imageRepository = imageRepository
+        
+        // FIXME: - 호출할 시점 고민
+        fetchPets()
+    }
+    
+    private func fetchPets() {
+        petRepository.fetch { result in
+            switch result {
+            case .success(let pets):
+                self.pets = pets
+            // FIXME:
+            case .failure(_): ()
+                
+            }
+        }
+    }
+}
+
+extension PetListPresenterImplementation: PetListPresenter {
+    var numberOfPets: Int {
+        return pets.count
     }
     
     func configure(view: PetView, at index: Int) {
